823ae536be1b37a8a3af8caf10e686c5c1ba74d4
AONE8 <malyshko.dp@gmail.com>
2026-03-02 13:13:37 +0200

    Correct device search data absence

--- src/libs/isFormDataEmpty.ts
+++ src/libs/isFormDataEmpty.ts
@@ -1,6 +1,6 @@
 export function isFormDataEmpty(formData: FormData): boolean {
-  for (const _ of formData.entries()) {
-    return false;
+  for (const value of formData.values()) {
+    if (value) return false;
   }
   return true;
 }
